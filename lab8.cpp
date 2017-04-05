dfdc00bcf6fc3a9ab8206156bd8f3ea1f9fe5281
Marshall Lindsay <mbanks1050@gmail.com>
2017-04-05 18:14:31 -0500

    Completed

--- lab8.cpp
+++ lab8.cpp
@@ -28,7 +28,7 @@ Prompts the user for a new message and adds that message
 to the msg string member
 */
 Message::Message(){
-	cout<<"Constructor"<<endl;
+	//cout<<"Constructor"<<endl;
 	string userInput;
 	cout<<"Please enter a message:"<<endl;
 	getline(cin, userInput);
@@ -43,7 +43,7 @@ Takes a string in as the message and adds that message to the
 msg string member;
 */
 Message::Message(string userInput){
-	cout<<"Constructor"<<endl;
+	//cout<<"Constructor"<<endl;
 	this -> msg = userInput;
 }
 
@@ -51,7 +51,7 @@ Message::Message(string userInput){
 Nothing in this class needs to be deleted;
 */
 Message::~Message(){
-	cout<<"Destructor"<<endl;
+	//cout<<"Destructor"<<endl;
 }
 
 void Message::print(){
@@ -64,6 +64,7 @@ class morseCodeMessage: public Message{
 	protected:
 		string morseMsg;
 	public:
+		morseCodeMessage* next;
 		morseCodeMessage();
 		morseCodeMessage(string);
 		~morseCodeMessage();
@@ -75,6 +76,7 @@ class morseCodeMessage: public Message{
 //and saves the translation to the extended morseMsg member
 morseCodeMessage::morseCodeMessage(){
 	translate(this -> msg);
+	this -> next = NULL;
 }
 
 //Parametric constructor. Takes a string as input. Calls the base
@@ -82,10 +84,11 @@ morseCodeMessage::morseCodeMessage(){
 //to morse code and saves it to the extended morseMsg member.
 morseCodeMessage::morseCodeMessage(string userInput) : Message(userInput){
 	translate(this -> msg);
+	this -> next = NULL;
 }
 
 morseCodeMessage::~morseCodeMessage(){
-	cout<<"Destructor"<<endl;
+	//cout<<"Destructor"<<endl;
 }
 
 void morseCodeMessage::translate(string input){
@@ -220,19 +223,98 @@ void morseCodeMessage::print(){
 	cout<< this->morseMsg <<endl;
 }
 
-int main(int argc, char* argv[]){
-	string userInput;
-	morseCodeMessage* newMessage;
-	if(argc == 1){
-		newMessage = new morseCodeMessage();
-	}else if(argc == 2){
-		newMessage = new morseCodeMessage(argv[1]);
-	}else{
-		cout<<"Too many command line arguments!"<<endl;
-		return(1);
+//-----------------------------------------------------------------
+//----------------------Generic Stack Class------------------------
+class messageStack{
+	private:
+		morseCodeMessage* top;
+	public:
+		messageStack();
+		~messageStack();
+		void push();
+		void pop();
+		void printStack();
+	
+};
+
+//Default constructor. Sets the pointer to the top of the
+//Stack to NULL.
+messageStack::messageStack(){
+	this -> top = NULL;
+}
+
+//Destructor. This will delete each object in the linked list.
+messageStack::~messageStack(){
+	morseCodeMessage* temp;
+	while(top != NULL){
+			temp = this -> top;
+			this -> top = this -> top -> next;
+			delete temp;
 	}
-		
-	newMessage->print();
-		
-	delete newMessage;
+}
+
+//Creates a new message and sets it to the top of the stack.
+void messageStack::push(){
+	morseCodeMessage* newNode = new morseCodeMessage();
+	newNode -> next = this -> top;
+	this -> top = newNode;
+}
+
+//Cycles down the stack and prints each message in turn.
+void messageStack::printStack(){
+	morseCodeMessage* msgPtr = this -> top;
+	while(msgPtr != NULL){
+		msgPtr -> print();
+		msgPtr = msgPtr -> next;
+	}
+	
+}
+
+//Deletes the top object from the stack. Sets the top of the
+//stack to the next one down.
+void messageStack::pop(){
+	if(top == NULL){
+		cout<<"\nThere is nothing on the stack!"<<endl;
+		return;
+	}
+	morseCodeMessage* retObj = this -> top;
+	this -> top = this -> top -> next;
+	delete retObj;
+}
+
+void optionMenu();
+void errorMessage();
+
+int main(int argc, char* argv[]){
+	messageStack stack;
+	
+	string userInput;
+	
+	while(1){
+		optionMenu();
+		getline(cin, userInput);
+		if(userInput == "1"){
+			stack.push();
+		}else if(userInput == "2"){
+			stack.pop();
+		}else if(userInput == "3"){
+			stack.printStack();
+		}else if(userInput == "4"){
+			return(0);
+		}else{
+			errorMessage();
+		}
+	}
+}
+
+void optionMenu(){
+	cout<<"\nWhat would you like to do?"<<endl;
+	cout<<"1) Push message to the stack\n"
+		<<"2) Pop message off the stack\n"
+		<<"3) Print all messages on the stack\n"
+		<<"4) Quit"<<endl;
+}
+
+void errorMessage(){
+	cout<<"Invalid input!"<<endl;
 }
